d2176d307761b9c1952c3ad38a74c4e1a78ce98f
Umer <wildoneshacker92@gmail.com>
2026-04-21 16:08:13 +0500

    day2

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
 app.use(express.json());
- 
-let tasks = [];
-let nextId = 1;
- 
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
- 
-app.get('/', (req, res) => {
-  res.send('API is running');
+app.get('/', (req, res) => { res.send('API is running'); });
+app.get('/tasks', async (req, res) => {
+  const result = await pool.query(
+    'SELECT * FROM tasks ORDER BY created_at DESC'
+  );
+  res.json(result.rows);
 });
-app.post('/tasks', (req, res) => {
-  const task = { id: nextId++, title: req.body.title, done: false };
-  tasks.push(task);
-  res.status(201).json(task);
+
+app.post('/tasks', async (req, res) => {
+  const { title } = req.body;
+
+  const result = await pool.query(
+    'INSERT INTO tasks (title) VALUES ($1) RETURNING *',
+    [title]
+  );
+
+  res.status(201).json(result.rows[0]);
 });
- 
-app.delete('/tasks/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  tasks = tasks.filter(t => t.id !== id);
+
+app.delete('/tasks/:id', async (req, res) => {
+  await pool.query(
+    'DELETE FROM tasks WHERE id = $1',
+    [req.params.id]
+  );
+
   res.json({ message: 'Deleted' });
 });
- 
-app.listen(3000, () => console.log('Server running on port 3000'));
+
+app.listen(3000, () => console.log('Server on port 3000'));
